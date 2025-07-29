0d88829a9adf22cbd5323b84929a231c832ecfd2
Viraj Dey <viraj65.dey@gmail.com>
2025-07-29 20:59:37 +0530

    latest commit

--- user.js
+++ user.js
@@ -7,7 +7,11 @@ function withCORS(response) {
   headers.set("Access-Control-Allow-Origin", "*");
   headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  return new Response(response.body, { ...response, headers });
+  return new Response(response.body, { 
+    status: response.status,
+    statusText: response.statusText,
+    headers 
+  });
 }
 
 // Database connection helper
